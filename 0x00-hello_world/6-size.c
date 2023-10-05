07a2e98d8d8f2a1ddbdf3ff211b47defca5bca47
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-05 14:41:26 +0100

    Script that prints the size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descripition: '6-size.c' print size of var
+ *
+ * Return: Alwyas 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
+
+	printf("Size of a char: %u byte(s)\n", sizeof(a));
+	printf("Size of an int: %u byte(s)\n", sizeof(b));
+	printf("Size of a long: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+
+	return (0);
+}

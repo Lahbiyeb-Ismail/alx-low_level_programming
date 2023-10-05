d6bc9f32a9f49dfe7036869d74c4439b4827e31c
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-05 14:43:17 +0100

    fix the output error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long c;
-	long long d;
-	float e;
+  char a;
+  int b;
+  long c;
+  long long d;
+  float e;
 
-	printf("Size of a char: %u byte(s)\n", sizeof(a));
-	printf("Size of an int: %u byte(s)\n", sizeof(b));
-	printf("Size of a long: %lu byte(s)\n", sizeof(c));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+  printf("Size of a char: %u byte(s)\n", sizeof(a));
+  printf("Size of an int: %u byte(s)\n", sizeof(b));
+  printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+  printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+  printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
-	return (0);
+  return (0);
 }

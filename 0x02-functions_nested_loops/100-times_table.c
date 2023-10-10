467b93ae90bd0fbb58f1b05b6ef4992b5fad7f98
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-10 17:16:43 +0100

    fix output error - 2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,24 +16,25 @@ void print_times_table(int n)
 {
 	int x, y, result;
 
-	if (n > 0 && n < 15)
+	if (n > 0 || n < 15)
+		return;
+
+	for (x = 0; x <= n; x++)
 	{
-		for (x = 0; x <= n; x++)
+		for (y = 0; y <= n; y++)
 		{
-			for (y = 0; y <= n; y++)
-			{
 
-				result = x * y;
+			result = x * y;
 
-				if (y == 0)
-					_putchar(result + '0');
+			if (y == 0)
+				_putchar(result + '0');
 
-				formatResult(result, y);
-			}
-			_putchar('\n');
+			formatResult(result, y);
 		}
+		_putchar('\n');
 	}
 }
+
 
 /**
  * formatResult - Entry point

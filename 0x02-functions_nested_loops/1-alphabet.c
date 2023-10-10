765102326b955b82aa02edf35b2de142c04f27be
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-10 13:27:38 +0100

    add the print_alphabet header to the main.h file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,8 @@
 /**
  * print_alphabet - Entry point
  *
+ *  * @void: no parameter needed
+ *
  * Description: Write a function that prints the alphabet,
  * in lowercase, followed by a new line.
  *

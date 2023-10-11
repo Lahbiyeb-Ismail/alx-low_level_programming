522abec143dc1cae253639b851040da6c000cccd
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-11 14:43:32 +0100

    error fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,16 +2,6 @@
 
 /**
 * main - causes an infinite loop
-*
-* Comment out (don’t delete it!) the part of the code that is
-* causing the  output to go into an infinite loop.
-* Don’t add or remove any lines of code, as we will be
-* checking your line * count. You are only allowed to
-* comment out existing code.
-* You do not have to compile with
-* -Wall -Werror -Wextra -pedantic for
-* this task.
-*
 * Return: 0
 */
 
@@ -23,12 +13,10 @@ int main(void)
 
 	i = 0;
 
-	/**
-	* while (i < 10)
+	/** while (i < 10)
 	* {
 	*	putchar(i);
-	* }
-	*/
+	* } */
 
 	printf("Infinite loop avoided! \\o/\n");
 

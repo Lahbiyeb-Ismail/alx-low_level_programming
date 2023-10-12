5bfe88ea7450e2bb8ff9cb3b2ec5ec727d838a97
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-12 18:47:27 +0100

    error fixed - 3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,44 +9,43 @@
 
 void print_number(int n)
 {
-	unsigned int _abs;
-	int ones, tens, hundreds, thousands;
+	unsigned int i;
+	int x, y, z, a;
 
 	if (n < 0)
 	{
-		_abs = n * -1;
+		i = n * -1;
 		_putchar('-');
 	}
 	else
 	{
-		_abs = n;
+		i = n;
 	}
+	x = i % 10;
+	y = ((i % 100) - x) / 10;
+	z = (i % 1000 - i % 100) / 100;
+	a = (i - (i % 1000)) / 1000;
 
-	ones = _abs % 10;
-	tens = ((_abs % 100) - ones) / 10;
-	hundreds = (_abs % 1000 - _abs % 100) / 100;
-	thousands = (_abs - (_abs % 1000)) / 1000;
-
-	if ((n <= 9 && n >= 0) || (n < 0 && n >= 9))
+	if ((n >= 0 && n <= 9) || (n < 0 && n >= -9))
 	{
-		_putchar(_abs + '0');
+		_putchar(i + '0');
 	}
 	else if ((n > 9 && n <= 99) || (n < -9 && n >= -99))
 	{
-		_putchar(tens + '0');
-		_putchar(ones + '0');
+		_putchar(y + '0');
+		_putchar(x + '0');
 	}
 	else if ((n > 99 && n <= 999) || (n < -99 && n >= -999))
 	{
-		_putchar(hundreds + '0');
-		_putchar(tens + '0');
-		_putchar(ones + '0');
+		_putchar(z + '0');
+		_putchar(y + '0');
+		_putchar(x + '0');
 	}
 	else if ((n > 999 && n <= 9999) || (n < -999 && n >= -9999))
 	{
-		_putchar(thousands + '0');
-		_putchar(hundreds + '0');
-		_putchar(tens + '0');
-		_putchar(ones + '0');
+		_putchar(a + '0');
+		_putchar(z + '0');
+		_putchar(y + '0');
+		_putchar(x + '0');
 	}
 }

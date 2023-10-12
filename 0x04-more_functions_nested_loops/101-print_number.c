c20072eb4176ad019334f7da253f686b389b842d
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-12 18:42:26 +0100

    error fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,27 +2,19 @@
 
 /**
  * print_number - Entry point
- *
  * @n: number to print
- *
  * Description: Function that prints an integer.
- *
- * Prototype: void print_number(int n);
- * You can only use _putchar function to print
- * You are not allowed to use long
- * You are not allowed to use arrays or pointers
- * You are not allowed to hard-code special values
- *
  * Return: nothing
  */
 
 void print_number(int n)
 {
 	unsigned int _abs;
+	int ones, tens, hundreds, thousands;
 
 	if (n < 0)
 	{
-		_abs = -n;
+		_abs = n * -1;
 		_putchar('-');
 	}
 	else
@@ -30,29 +22,31 @@ void print_number(int n)
 		_abs = n;
 	}
 
+	ones = _abs % 10;
+	tens = ((_abs % 100) - ones) / 10;
+	hundreds = (_abs % 1000 - _abs % 100) / 100;
+	thousands = (_abs - (_abs % 1000)) / 1000;
+
 	if ((n <= 9 && n >= 0) || (n < 0 && n >= 9))
 	{
 		_putchar(_abs + '0');
 	}
-
 	if ((n > 9 && n <= 99) || (n < -9 && n >= -99))
 	{
-		_putchar((_abs / 10) + '0');
-		_putchar((_abs % 10) + '0');
+		_putchar(tens + '0');
+		_putchar(ones + '0');
 	}
-
 	if ((n > 99 && n <= 999) || (n < -99 && n >= -999))
 	{
-		_putchar((_abs / 100) + '0');
-		_putchar((_abs / 10 % 10) + '0');
-		_putchar((_abs % 10) + '0');
+		_putchar(hundreds + '0');
+		_putchar(tens + '0');
+		_putchar(ones + '0');
 	}
-
 	if ((n > 999 && n <= 9999) || (n < -999 && n >= -9999))
 	{
-		_putchar((_abs / 1000) + '0');
-		_putchar((_abs / 100 % 10) + '0');
-		_putchar((_abs / 10 % 10) + '0');
-		_putchar((_abs % 10) + '0');
+		_putchar(thousands + '0');
+		_putchar(hundreds + '0');
+		_putchar(tens + '0');
+		_putchar(ones + '0');
 	}
 }

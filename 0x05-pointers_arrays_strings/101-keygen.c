73f03c0ce1968c2787e48f6ef0b6f85e9912256f
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-16 18:06:27 +0100

    C program that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - check the code
+ *
+ * Description: C program that generates random valid
+ * passwords for the program 101-crackme.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int my_rand;
+	int count;
+	int total;
+
+	srand(time(NULL));
+
+	for (count = 0, total = 1337; total > 122; count++)
+	{
+		my_rand = rand() % 125;
+		printf("%c", my_rand);
+		total -= my_rand;
+	}
+	printf("%c", total);
+	printf("\n");
+
+	return (0);
+}

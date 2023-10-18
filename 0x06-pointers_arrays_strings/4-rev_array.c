eb2c811321ea55b023762dcb5d0d00b4e83908a9
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-18 14:35:17 +0100

    C function that reverses the content of an array of integers.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ *
+ *  * @a: pointer to an array of integers.
+ *  * @n: the number of elements of the array
+ *
+ *
+ * Description: Function that reverses the content of an
+ * array of integers.
+ *
+ *
+ * Prototype: void reverse_array(int *a, int n);
+ *
+ *
+ * Return: void
+ *
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, tmp_val;
+	int last_idx = n - 1;
+
+	while (i <= n / 2)
+	{
+
+		tmp_val = a[i];
+		a[i] = a[last_idx];
+		a[last_idx] = tmp_val;
+
+		i++;
+		last_idx--;
+	}
+}

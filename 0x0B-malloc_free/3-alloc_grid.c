6963ca0768a0d32689f0712d21e0bbd3ae9cb445
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-10-31 15:20:30 +0100

    add the free allocated memory functionality

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -43,7 +43,14 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+
+			free(grid);
 			return (NULL);
+
+		}
 	}
 
 	for (i = 0; i < height; i++)

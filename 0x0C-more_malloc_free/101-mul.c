f916d3509bc9022cdaf2ae9ac78226f8c693a6a4
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-11-02 17:46:18 +0100

    add memory allocation

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,8 @@
 
 void _puts(char *str);
 int _atoi(char *s);
-void print_int(unsigned long int n);
+int is_valid_number(char *s);
+char *multiply_strings(char *s1, char *s2);
 
 /**
  * main - Entry point
@@ -21,22 +22,26 @@ void print_int(unsigned long int n);
 
 int main(int argc, char *argv[])
 {
-	long int res, num1, num2;
+	char *res;
 
-	if (argc != 3)
+	if (argc != 3 || !is_valid_number(argv[1]) || !is_valid_number(argv[2]))
 	{
 		_puts("ERROR");
 
 		exit(98);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
+	res = multiply_strings(argv[1], argv[2]);
 
-	res = num1 * num2;
+	if (res == NULL)
+	{
+		_puts("ERROR");
+		exit(98);
+	}
 
-	print_int(res);
-	_putchar('\n');
+	_puts(res);
+
+	free(res);
 
 	return (0);
 }
@@ -111,28 +116,119 @@ int _atoi(char *s)
 }
 
 /**
- * print_int - Entry point
+ * is_valid_number - Entry point
  *
- *  * @n: int
+ *  * @s: string
  *
- * Description: Function print an integer
+ * Description: Function that check if a given string is a valid number
  *
  *
  * Return: void
  *
  */
 
-void print_int(unsigned long int n)
+int is_valid_number(char *s)
 {
-	unsigned long int divisor = 1, i, res;
+	int i;
 
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-		;
+	if (!s || !s[0])
+		return (0);
 
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	i = 0;
+
+	while (s[i])
 	{
-		res = n / divisor;
-		_putchar(res + '0');
+		if (i == 0 && s[i] == '-')
+			i++;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
 
+	return (1);
+}
+
+/**
+ * multiply_strings - Entry point
+ *
+ *  * @s1: string
+ *  * @s2: string
+ *
+ * Description: Function that multiplies two string number
+ *
+ *
+ * Return: Result of s1 * s2
+ *
+ */
+
+char *multiply_strings(char *s1, char *s2)
+{
+	int num1 = _atoi(s1);
+	int num2 = _atoi(s2);
+	int result = num1 * num2;
+	int result_length = 0, sign, i, left, right;
+	int temp_result = result;
+	char *result_str;
+
+	if (temp_result == 0)
+		result_length = 1;
+	else
+	{
+		while (temp_result != 0)
+		{
+			result_length++;
+			temp_result /= 10;
+		}
+	}
+
+	sign = 1;
+
+	if (result < 0)
+	{
+		sign = -1;
+		result_length--;
+	}
+
+	result_str = malloc((result_length + 1) * sizeof(char));
+
+	if (result_str == NULL)
+		return (NULL);
+
+
+	i = 0;
+
+	if (sign == -1)
+		result_str[i++] = '-';
+
+
+	if (result == 0)
+		result_str[i] = '0';
+
+	else
+	{
+		while (result != 0)
+		{
+			int digit = result % 10;
+
+			result_str[i++] = ('0' + digit);
+			result /= 10;
+		}
+	}
+
+	result_str[i] = '\0';
+
+	left = sign == -1 ? 1 : 0;
+	right = i - 1;
+
+	while (left < right)
+	{
+		char temp = result_str[left];
+
+		result_str[left] = result_str[right];
+		result_str[right] = temp;
+		left++;
+		right--;
+	}
+
+	return (result_str);
 }

eadc91b05c166e8187c1eac09697e16374ac1cb4
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-11-09 14:51:20 +0100

    add the correct output when string == NNULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -37,7 +37,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		 * str == NULL ? printf("nil") : printf("%s", str);
 		*/
 
-		printf("%s", str == NULL ? "nil" : str);
+		printf("%s", str == NULL ? "(nil)" : str);
 
 		if (i != n - 1 && separator)
 			printf("%s", separator);

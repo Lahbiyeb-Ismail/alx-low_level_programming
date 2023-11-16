b840f559e4b3d7b55c6754f93196434893a22901
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-11-16 15:16:59 +0100

    add comments to the code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -23,31 +23,39 @@ list_t *add_node_end(list_t **head, const char *str)
 	int i = 0;
 
 	new_node = malloc(sizeof(list_t));
-
 	if (!new_node)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	while (str[i])
 		i++;
-
+	/**
+	 * Create a new node with the given data.
+	 * And make the new_node->next = NULL.
+	 * (Because the new node is going to be the last node.)
+	*/
 	new_node->str = strdup(str);
 	new_node->len = i;
 	new_node->next = NULL;
-
+	/**
+	 * If the head node is NULL (Empty Linked List)
+	 * make the new_node as the head.
+	*/
 	if (*head == NULL)
 		*head = new_node;
+	/**
+	 * Else if the head node is not null,
+	 * (Linked list already has some elements)
+	*/
 	else
 	{
 		last_node = *head;
-
+		/* find the last node. */
 		while (last_node->next)
 			last_node = last_node->next;
-
+		/* make the last_node->next as the new_node. */
 		last_node->next = new_node;
 	}
-
 	return (last_node);
 }

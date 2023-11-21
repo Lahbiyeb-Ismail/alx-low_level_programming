0bbdd4324bcd1e2d7efcd936cc7de494cd8b316d
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-11-21 13:37:22 +0100

    Task 10 -- code refactoring

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *curr_node, *new_node;
 	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -55,8 +55,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			i++;
 		}
 
-		new_node->next = curr_node->next;
+		/* Index out of bound */
+		if (!curr_node)
+		{
+			free(new_node);
+			return (NULL);
+		}
 
+		new_node->next = curr_node->next;
 		curr_node->next = new_node;
 	}
 

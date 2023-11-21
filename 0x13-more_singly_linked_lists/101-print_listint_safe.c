2322fb0ebdf6abd309c4e70a77a38f2db86122ca
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2023-11-21 16:20:32 +0100

    C function that prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,35 @@
 #include "lists.h"
 
 /**
+ * free_list - Entry point
+ *
+ * * @head: A pointer to the head of the list.
+ *
+ * * Description:
+ * This function takes a pointer to the head of a linked list and frees
+ * the memory allocated for each node in the list. It sets the head
+ * pointer to NULL after freeing the entire list.
+ *
+ * * Return: Void
+ */
+
+void free_list(listp_t **head)
+{
+	listp_t *curr_node, *next_node;
+
+	if (head != NULL)
+	{
+		curr_node = *head;
+		while ((next_node = curr_node) != NULL)
+		{
+			curr_node = curr_node->next;
+			free(next_node);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * print_listint_safe - Entry point
  *
  * * @head: linked list head
@@ -17,17 +46,39 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	int i = 0;
+	size_t nodes_count = 0;
+	listp_t *visited_node, *curr_node, *new_node;
 
-	if (!head)
-		exit(98);
-
+	visited_node = NULL;
 	while (head != NULL)
 	{
+		curr_node = malloc(sizeof(listp_t));
+
+		if (curr_node == NULL)
+			exit(98);
+
+		curr_node->p = (void *)head;
+		curr_node->next = visited_node;
+		visited_node = curr_node;
+
+		new_node = visited_node;
+
+		while (new_node->next != NULL)
+		{
+			new_node = new_node->next;
+			if (head == new_node->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_list(&visited_node);
+				return (nodes_count);
+			}
+		}
+
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
-		i++;
+		nodes_count++;
 	}
 
-	return (i);
+	free_list(&visited_node);
+	return (nodes_count);
 }
